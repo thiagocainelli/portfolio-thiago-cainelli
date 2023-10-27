a621116672b25c020988f554a5fe5ef92ed61984
Thiago Cainelli <thiagocainelli@gmail.com>
2023-10-27 18:07:50 -0300

    Added scroll smooth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,7 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      scrollBehavior: ['responsive', 'scroll'],
     },
   },
   plugins: [require("tailwindcss-animate")],
